19f245878a3f94badd3f43ddd758b5a7551022c6
Ben Snow <roskoe432@gmail.com>
2019-02-01 12:03:43 -0500

    Cleaned up my code a tad bit

--- week3-homework/hw.js
+++ week3-homework/hw.js
@@ -1,130 +1,98 @@
-/*-----------------------------------------------------------------------------------
-PART II
+const HW = (function () {
+	function HW() { }
 
-Part II will focus on Javascript's ability to manipulate the DOM.
-Use the provided index.html
------------------------------------------------------------------------------------*/
+	// 1
+	HW.getUSA = function () {
+		let spans = document.getElementsByTagName('span');
+		for (let i = 0; i < spans.length; i++) {
+			if (spans[i].innerHTML === 'USA') {
+				console.log(spans[i].innerHTML)
+			}
+		}
+	};
 
-/* 1. USA
-Define function getUSA()
-Find the html element that contains "USA".
-Print that element's contents. */
-function getUSA() {
-	let spans = document.getElementsByTagName('span');
-	for(let i = 0; i < spans.length; i++) {
-		if(spans[i].innerHTML === 'USA') {
-			console.log(spans[i].innerHTML)
+	// 2
+	HW.getPeopleInSales = function () {
+		let td = document.querySelectorAll('[class="empName"]');
+		for (let i = 0; i < td.length; i++)
+			console.log(td[i].innerHTML);
+	};
+
+	// 3
+	HW.getAnchorChildren = function () {
+		let spans = document.querySelectorAll('a > span');
+		for (let i = 0; i < spans.length; i++) {
+			console.log(spans[i].innerHTML);
+		}
+	};
+
+	// 4
+	HW.getHobbies = function () {
+		let hobbies = document.querySelector('[name="hobbies"]');
+		let opts = hobbies.getElementsByTagName('option');
+		for (let i = 0; i < opts.length; i++) {
+			let selected = opts[i].getAttribute('selected') === 'selected';
+			if (selected) {
+				let val = opts[i].getAttribute('value');
+				console.log(`Content: ${opts[i].innerHTML}, Value: ${val}`);
+			}
+		}
+	};
+
+	// 5
+	HW.getCustomAttribute = function () {
+		let cust = document.querySelectorAll('[data-customAttr]');
+		for (let i = 0; i < cust.length; i++) {
+			let value = cust[i].getAttribute('data-customAttr');
+			console.log(`Value = ${value}, ${cust[i]}`);
 		}
 	}
-}
-getUSA();
 
-
-
-/* 2. Sales
-Define function getPeopleInSales()
-Print the names of all the people in the sales department. */
-function getPeopleInSales() {
-	let td = document.querySelectorAll('[class="empName"]');
-	for(let i = 0; i < td.length; i++)
-		console.log(td[i].innerHTML);
-}
-getPeopleInSales();
-
-
-
-/* 3. Click Here
-Define function getAnchorChildren()
-Find all anchor elements with a <span> child.
-Print the contents of <span> */
-function getAnchorChildren() {
-	let spans = document.querySelectorAll('a > span');
-	for(let i = 0; i < spans.length; i++) {
-		console.log(spans[i].innerHTML);
-	}
-}
-getAnchorChildren();
-
-
-
-/* 4. Hobbies
-Define function getHobbies()
-Find all checked options in the 'skills' select element.
-Print the value and the contents. */
-function getHobbies() {
-	let hobbies = document.querySelector('[name="hobbies"]');
-	let opts = hobbies.getElementsByTagName('option');
-	for(let i = 0; i < opts.length; i++) {
-		let selected = opts[i].getAttribute('selected') === 'selected';
-		if(selected) {
-			let val = opts[i].getAttribute('value');
-			console.log(`Content: ${opts[i].innerHTML}, Value: ${val}`);
+	// 10
+	HW.walkTheDom = function(node, func) {
+		func(node);
+		if (node.childElementCount > 0) {
+			for (let i = 0; i < node.children.length; i++) {
+				WalkTheDom(node.children[i], func);
+			}
 		}
-	}
-}
-getHobbies();
+	};
+
+	return HW;
+})();
 
 
 
-/* 5. Custom Attribute
-Define function getCustomAttribute()
-Find all elements with "data-customAttr" attribute
-Print the value of the attribute.
-Print the element that has the attribute. */
-function getCustomAttributes() {
-	let cust = document.querySelectorAll('[data-customAttr]');
-	for(let i = 0; i < cust.length; i++) {
-		let value = cust[i].getAttribute('data-customAttr');
-		console.log(`Value = ${value}, ${cust[i]}`);
-	}
-}
-getCustomAttributes();
+// 6
+(function () {
+	const rgx = new RegExp('^[0-9]*$');
+	const num1 = document.getElementById('num1');
+	const num2 = document.getElementById('num2');
+	const sum = document.getElementById('sum');
 
-
-
-/* 6. Sum Event
-NOTE: Write unobtrusive Javascript
-Regarding these elements:
-	<input id="num1" class="nums" type="text"/>
-	<input id="num2" class="nums" type="text"/>
-	<h3>Sum: <span id="sum"></span></h3> 
-
-Define onchange event handler.
-Add <input> element values.
-Put the sum in the <span> element.
-If values cannot be added, put "Cannot add" in the <span> element*/
-function OnSumInputValues() {
-	let num1 = document.getElementById('num1');
-	let num2 = document.getElementById('num2');
-	let span = document.getElementById('sum');
-
-	num1.value = '5';
-	num2.value = '10';
-	num1.setAttribute('type', 'number');
-	num2.setAttribute('type', 'number');
-	span.innerHTML = '15';
+	num1.value = '0';
+	num2.value = '0';
+	sum.innerHTML = '0';
 
 	function add(ev) {
-		sum.innerHTML = 0;
-		sum.innerHTML = +num1.value + +num2.value;
+		if(rgx.test(num1.value) && rgx.test(num2.value)) {
+			sum.innerHTML = +num1.value + +num2.value;
+			return;
+		}
+		sum.innerHTML = 'Cannot Add';
 	}
 
-	num1.addEventListener('change', add);
-	num2.addEventListener('change', add);
-}
-// SumEvent();
+	num1.addEventListener('input', add);
+	num2.addEventListener('input', add);
+})();
 
 
 
-/* 7. Skills Event
-NOTE: Write unobtrusive Javascript
-When user selects a skill, create an alert with a message similar to:
-	"Are you sure CSS is one of your skills?"
-NOTE: no alert should appear when user deselects a skill. */
-function OnSkillSelect() {
-	let select = document.getElementsByTagName('select');
-	for(let i = 0; i < select.length; i++) {
-		if(select[i].getAttribute('name', 'skills')) {
+// 7
+(function() {
+	const select = document.getElementsByTagName('select');
+	for (let i = 0; i < select.length; i++) {
+		if (select[i].getAttribute('name', 'skills')) {
 			let id = select[i].value;
 			select[i].addEventListener('change', e => {
 				let ans = prompt(`Are you sure ${select[i].value} is one of your skills? (yes or no)`);
@@ -135,29 +103,21 @@ function OnSkillSelect() {
 			});
 		}
 	}
-}
-// OnSkillSelect();
+})();
 
 
 
-/* 8. Favorite Color Event
-NOTE: Write unobtrusive Javascript
-NOTE: This is regarding the favoriteColor radio buttons.
-When a user selects a color, create an alert with a message similar to:
-	"So you like green more than blue now?"
-In this example, green is the new value and blue is the old value.
-Make the background color (of all favoriteColor radio buttons) the newly selected favoriteColor
- */
-function OnColorSelect() {
-	let favColors = document.querySelectorAll('form#firstForm input[name="favoriteColor"]');
+// 8
+(function() {
+	const favColors = document.querySelectorAll('form#firstForm input[name="favoriteColor"]');
 	let last = null, current = null;
-	for(let i = 0; i < favColors.length; i++) {
+	for (let i = 0; i < favColors.length; i++) {
 		favColors[i].addEventListener('change', (e) => {
 			current = favColors[i];
-			if(last === null) {
+			if (last === null) {
 				last = current;
 			} else {
-				if(current != last) {
+				if (current != last) {
 					let a = current.getAttribute('value');
 					let b = last.getAttribute('value');
 					alert(`So you like ${a} more than ${b} now.`);
@@ -167,19 +127,14 @@ function OnColorSelect() {
 			document.body.style.backgroundColor = current.getAttribute('value');
 		});
 	}
-}
-// OnColorSelect();
+})();
 
 
 
-/* 9. Show/Hide Event
-NOTE: Write unobtrusive Javascript
-When user hovers over an employees name:
-	Hide the name if shown.
-	Show the name if hidden. */
-function ChangeVisibilityOfEmpName() {
-	let td = document.querySelectorAll('[class="empName"]');
-	for(let i = 0; i < td.length; i++) {
+// 9
+(function() {
+	const td = document.querySelectorAll('[class="empName"]');
+	for (let i = 0; i < td.length; i++) {
 		let style = getComputedStyle(td[i]);
 		let last = style.opacity;
 		td[i].addEventListener('mouseover', (e) => {
@@ -187,37 +142,31 @@ function ChangeVisibilityOfEmpName() {
 			td[i].style.opacity = (isVis ? '0' : last);
 		});
 	}
-}
-// ChangeVisibilityOfEmpName();
+})();
 
 
 
-/* 10. Current Time
-Regarding this element:
-	<h5 id="currentTime"></h5>
-Show the current time in this element in this format: 9:05:23 AM
-The time should be accurate to the second without having to reload the page.
- */
-function PrintTime() {
-	let h5 = document.getElementById('currentTime');
-	function UpdateTime() {
-		h5.innerHTML = new Date().toLocaleTimeString();
-		let id = setTimeout(UpdateTime, 1000);
-	}
-	return UpdateTime();
-}
-// PrintTime();
+// 10
+(function() {
+	window.addEventListener('load', (e) => {
+		function PrintTime() {
+			let h5 = document.getElementById('currentTime');
+			function UpdateTime() {
+				h5.innerHTML = new Date().toLocaleTimeString();
+				let id = setTimeout(UpdateTime, 1000);
+			}
+			return UpdateTime();
+		}
+		PrintTime();
+	});
+})();
 
 
 
- /* 11. Delay
-Regarding this element:
-	<p id="helloWorld">Hello, World!</p>
-Three seconds after a user clicks on this element, change the text to a random color.
- */
-let HelloWorld = (function() {
+// 11
+(function() {
 	function RandRange(min, max) {
-		return Math.round( (min + (max - min) * Math.random()) ).toString();
+		return Math.round((min + (max - min) * Math.random())).toString();
 	}
 	function RandColor() {
 		let r = RandRange(0, 255);
@@ -226,36 +175,13 @@ let HelloWorld = (function() {
 		return `rgb(${r}, ${g}, ${b})`;
 	}
 
-	return function() {
-		let p = document.getElementById('helloWorld');
-		let id = 0;
-		p.addEventListener('click', (e) => {
-			console.log('Clicked');
-			clearTimeout(id);
-			id = setTimeout(() => {
-				p.style.color = RandColor();
-			}, 3000);
-		});
-	};
+	let p = document.getElementById('helloWorld');
+	let id = 0;
+	p.addEventListener('click', (e) => {
+		console.log('Clicked');
+		clearTimeout(id);
+		id = setTimeout(() => {
+			p.style.color = RandColor();
+		}, 3000);
+	});
 })();
-// HelloWorld();
-
-
- /* 12. Walk the DOM
-Define function walkTheDOM(node, func)
-This function should traverse every node in the DOM. Use recursion.
-On each node, call func(node). */
-let WalkTheDom = (function() {
-	function WalkTheDom(node, func) {
-		func(node);
-		if (node.childElementCount > 0) {
-			for (let i = 0; i < node.children.length; i++) {
-				WalkTheDom(node.children[i], func);
-			}
-		}
-	}
-	return WalkTheDom;
-})();
-WalkTheDom(document.getElementsByTagName('html')[0], (node) => {
-	console.log(node.tagName);
-});
